bc90ba121eb70097fe05378567376c56fc2ebfe1
gunel93 <s339490@oslomet.no>
2021-05-04 18:35:51 +0200

    template folder ble lagt inn

--- App/app.py
+++ App/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
-#import mysql.connector
+import mysql.connector
 
-"""mydb = mysql.connector.connect(host="localhost", 
-                                user="root",   
-                                passwd="root", 
-                                database="the_shop")
+# from sqlalchemy.dialects import mysql
+"""mydb = mysql.connector.connect(host="localhost",
+                               user="root",
+                               passwd="root",
+                               database="the_shop")
 my_cursor = mydb.cursor()
 my_cursor.execute("SELECT * FROM the_shop")
 my_result = my_cursor.fetchall()"""
@@ -18,4 +19,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="127.0.0.1", debug=True)
