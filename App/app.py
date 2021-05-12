5b2c636702e6dd84e801fae711879682d7c80466
gunel93 <s339490@oslomet.no>
2021-05-12 19:57:12 +0200

    Addcart function doesn't work yet. I get url was not found error.

--- App/app.py
+++ App/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, session, redirect
 import mysql.connector
-import pymysql
+from mysql.connector.cursor import MySQLCursor
 
 # remember to adjust
 mydb = mysql.connector.connect(host="localhost",
@@ -28,6 +28,7 @@ def product(pid):
     my_result = my_cursor.fetchone()
     return render_template("products/prod.html", pid=pid, row=my_result)
 
+
 @app.route('/product/<int:pid>', methods=['GET'])
 def productToCart(pid):
     my_cursor = mydb.cursor()
@@ -36,37 +37,36 @@ def productToCart(pid):
     return render_template("products/prod.html", pid=pid, row=my_result)
 
 
-@app.route('/addcart', methods=['POST'])
-def addCart():
+@app.route('/addcart', methods=['POST', 'GET'])
+def AddCart():
     try:
-        product_id = int(request.form.get('product_id', int))
-        quantity = int(request.form.get('quantity', int))
+
+        product_id = int(request.form.get('product_id'))
+        quantity = int(request.form.get('quantity'))
+        my_cursor = mydb.cursor()
+        product = my_cursor.query.filter_by(id=product_id).first()
+
         if product_id and quantity and request.method == "POST":
-            dictItem = productToCart(product_id)
-            if 'Shoppingcart' in session:
-               print(session['Shoppingcart'])
-               print("Test in if statement")
+
+            dictItem = {product_id: {'name': product.pName, 'description': product.description, 'price': product.price,
+            'picture': product.picture, 'quantity': quantity}}
+
+            if 'Shoppcart' in session:
+                print(session['Shoppingcart'])
+
 
             else:
-             session['Shoppincart'] = dictItem
-             return redirect(request.referrer)
-            print("Test in else statement")
+                session['Shoppingcart'] = dictItem
+                return redirect(request.referrer)
+
     except Exception as e:
-              print(e)
-              print("Test in except statement")
+        print(e)
+        print("Test in except statement")
 
     finally:
-            return redirect(request.referrer)
-
-
-
-
-
+        return redirect(request.referrer)
 
 
 if __name__ == '__main__':
     app.run(host="localhost", debug=True)
     mydb.close()
-
-
-
