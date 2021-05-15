66d47747ca9bffa76909f3003cecf9da90019b04
gunel93 <s339490@oslomet.no>
2021-05-15 12:01:42 +0200

    addtocart funskjonen doesn't work, and I get url not found error

--- App/app.py
+++ App/app.py
@@ -33,49 +33,42 @@ def product(pid):
 @app.route('/product/<int:pid>', methods=['GET'])
 def productToCart(pid):
     my_cursor = mydb.cursor()
-    my_cursor.execute("SELECT pName, description, price, picture FROM products WHERE id = %d" % pid)
+    my_cursor.execute("SELECT pName,description,price,picture FROM products WHERE id = %d" % pid)
     my_result = my_cursor.fetchone()
     return render_template("products/prod.html", pid=pid, row=my_result)
 
 
 @app.route('/addtocart', methods=['POST', 'GET'])
 def AddCart():
-    Cursor = None
     try:
 
         product_id = int(request.form.get('product_id'))
         quantity = int(request.form.get('quantity'))
-        my_cursor = mydb.cursor()
-        product = my_cursor.query.filter_by(id=product_id).first()
+
+
 
         if product_id and quantity and request.method == "POST":
 
-            # conn = mysql.connect()
-            # cursor = conn.cursor(pymysql.cursors.DictCursor)
-            # cursor.execute("SELECT * FROM products where id=%d" % product_id)
-            # row = cursor.fetchone()
 
-            # dictItem = {row['id']: {'name': row['pName'], 'description': row['description'], 'price': row['price'],
-            # 'picture':row['picture'], 'quantity':quantity}}
-
-            dictItem = {product_id: {'name': product.pName, 'description': product.description, 'price': product.price,
-                                     'picture': product.picture, 'quantity': quantity}}
 
             dictItem = productToCart(product_id)
-            if 'Shoppcart' in session:
+
+            if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
 
-
             else:
-                session['Shoppingcart'] = dictItem
-                return redirect(url_for('index'))
+                     session['Shoppingcart'] = dictItem
+                     return redirect(request.referrer)
+
 
     except Exception as e:
         print(e)
-        print("Test in except statement")
+
 
     finally:
         return redirect(request.referrer)
+
+
 
 
 if __name__ == '__main__':
