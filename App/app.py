31e8771caa767dcd6b43618f735d2e868ae59d89
ainakatrine <s338849@oslomet.no>
2021-05-15 15:28:57 +0200

    Some small adjustments. Started making cart.

--- App/app.py
+++ App/app.py
@@ -43,7 +43,6 @@ def product(pid):
 def add_to_cart():
     try:
         product_id = request.form.get('product_id')
-
         qty = request.form.get('quantity')
 
         my_cursor = mydb.cursor()
@@ -75,6 +74,20 @@ def add_to_cart():
         return redirect(request.referrer)
 
 
+@app.route('/cart')
+def cart():
+    # if 'ShoppingCart' not in session or len(session['ShoppingCart']) <= 0:
+    #     return redirect(url_for('home'))
+    #
+    # subtotal = 0
+    # for key, my_product in session['ShoppingCart'].items():
+    #     subtotal += int(my_product[3]) * int(my_product['quantity'])
+    if 'ShoppingCart' not in session:
+        return redirect(request.referrer)
+
+    return render_template('products/cart.html')
+
+
 def merging_arrays(array, other_array):
     if isinstance(array, list) and isinstance(other_array, list):
         return array + other_array
@@ -84,7 +97,6 @@ def merging_arrays(array, other_array):
         return array.union(other_array)
     else:
         return False
-
 
 
 if __name__ == '__main__':
